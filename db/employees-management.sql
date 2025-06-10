9412d729ce249250fe55bdf1a4a061ef16c06405
Pavel <paulavetspavel@outlook.com>
2025-06-10 09:38:33 +0300

    New export db

--- db/employees-management.sql
+++ db/employees-management.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: localhost
--- Время создания: Июн 09 2025 г., 08:00
+-- Время создания: Июн 10 2025 г., 06:37
 -- Версия сервера: 8.0.36
 -- Версия PHP: 7.4.33
 
@@ -30,12 +30,13 @@ SET time_zone = "+00:00";
 CREATE TABLE `employees` (
   `id` int NOT NULL,
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `birth_date` date DEFAULT NULL,
   `position` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `hire_date` date NOT NULL,
   `salary` decimal(10,2) DEFAULT NULL,
   `photo_url` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `created_by` int DEFAULT NULL,
-  `status` enum('работает','в отпуске','в декрете','уволен','командировка') COLLATE utf8mb4_general_ci DEFAULT 'работает',
+  `status` enum('работает','в отпуске','в декрете','уволен','командировка') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT 'работает',
   `contract_end` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -43,11 +44,11 @@ CREATE TABLE `employees` (
 -- Дамп данных таблицы `employees`
 --
 
-INSERT INTO `employees` (`id`, `name`, `position`, `hire_date`, `salary`, `photo_url`, `created_by`, `status`, `contract_end`) VALUES
-(1, 'Иванов Иван Иванович', 'Директор', '2025-01-27', 10000.00, '/uploads/employee-1749454312365-647773329.jpg', 1, 'работает', '2027-06-01'),
-(2, 'Петров Петр Петрович', 'Бухгалтер', '2025-01-26', 7000.00, '', 1, 'в отпуске', '2027-01-26'),
-(3, 'Сидорова Мария Ивановна', 'Менеджер', '2025-02-01', 5000.00, '/uploads/employee-1749454433280-523700951.png', 1, 'в декрете', '2028-02-01'),
-(4, 'Павловец Павел Викторович', 'Программист', '2025-05-31', 5000.00, '/uploads/employee-1749454450131-540053084.jpg', 1, 'командировка', '2026-06-01');
+INSERT INTO `employees` (`id`, `name`, `birth_date`, `position`, `hire_date`, `salary`, `photo_url`, `created_by`, `status`, `contract_end`) VALUES
+(1, 'Иванов Иван Иванович', '1980-06-11', 'Директор', '2025-01-27', 10000.00, '/uploads/employee-1749454312365-647773329.jpg', 1, 'работает', '2027-06-01'),
+(2, 'Петров Петр Петрович', '1985-01-01', 'Бухгалтер', '2025-01-26', 7000.00, '', 1, 'в отпуске', '2027-01-26'),
+(3, 'Сидорова Мария Ивановна', '2005-05-20', 'Менеджер', '2025-02-01', 5000.00, '/uploads/employee-1749454433280-523700951.png', 1, 'в декрете', '2028-02-01'),
+(4, 'Павловец Павел Викторович', '1984-06-14', 'Программист', '2025-05-30', 6000.00, '/uploads/employee-1749535117373-827249026.jpg', 1, 'командировка', '2026-05-31');
 
 -- --------------------------------------------------------
 
@@ -91,7 +92,8 @@ INSERT INTO `logs` (`id`, `user_id`, `action`, `timestamp`) VALUES
 (22, 1, 'Updated employee with ID 2', '2025-06-09 07:43:10'),
 (23, 1, 'Updated employee with ID 2', '2025-06-09 07:49:57'),
 (24, 1, 'Updated employee with ID 2', '2025-06-09 07:50:40'),
-(25, 1, 'Updated employee with ID 2', '2025-06-09 07:53:59');
+(25, 1, 'Updated employee with ID 2', '2025-06-09 07:53:59'),
+(26, 1, 'Updated employee with ID 4', '2025-06-10 05:58:37');
 
 -- --------------------------------------------------------
 
@@ -110,7 +112,7 @@ CREATE TABLE `tokens` (
 --
 
 INSERT INTO `tokens` (`id`, `user_id`, `refresh_token`) VALUES
-(4, 2, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZW1haWwiOiJ1c2VyQHVzZXIuY29tIiwicGFzc3dvcmRfaGFzaCI6IiQyYiQwNCRFeGJ4WUp0NkxCOVEuaWNMOUZmc1guOVNndkVSbTljUnRSNExreHIxaEE5WUk2cEExVkphYSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzQ5NDU1OTU1LCJleHAiOjE3NTIwNDc5NTV9.C59TnxhGoArAV2UVMHTBOexV4nmkV_AW8PJGNF2IhnM');
+(5, 1, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJhZG1pbkBhZG1pbi5jb20iLCJwYXNzd29yZF9oYXNoIjoiJDJiJDA0JC5zczFXVlVJNkpHN0U0ZTdicTJ0MGVKb2V6ZXFJSmJKNUx0MEhwQUJJTGU1Ym9pbjBOejdXIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNzQ5NTM3MTQ0LCJleHAiOjE3NTIxMjkxNDR9.f-v7ksVEml8NlT-Xe25W3EyThEmvMNejBO5g168Oszs');
 
 -- --------------------------------------------------------
 
@@ -178,13 +180,13 @@ ALTER TABLE `employees`
 -- AUTO_INCREMENT для таблицы `logs`
 --
 ALTER TABLE `logs`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT для таблицы `tokens`
 --
 ALTER TABLE `tokens`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
